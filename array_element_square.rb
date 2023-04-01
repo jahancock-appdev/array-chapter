726e4d9df3359d59f56fceee7c59d6ba3314d470
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-01 04:32:58 +0000

    Final

--- array_element_square.rb
+++ array_element_square.rb
@@ -1,13 +1,15 @@
 # Print the square of the second-to-last number that the user enters.
-# 
+#
 # The expected output will be something like:
-# 
+#
 # Enter at least 2 numbers, separated by spaces:
 # 9.0
-# 
+#
 
 p "Enter at least 2 numbers, separated by spaces:"
 
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+p user_numbers[-2].to_f ** 2
